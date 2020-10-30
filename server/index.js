424ee407460b64d41ad05e52341bfda41c15c45d
Trung Trinh <trung28899@gmail.com>
2020-10-30 18:09:40 -0400

    server basic settings

--- server/index.js
+++ server/index.js
@@ -2,3 +2,30 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const cors = require("cors");
+
+const app = express();
+
+/*
+    Setting up body parser and cors
+    Setting limit because will send some image
+*/
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+// get from mongodb
+const CONNECTION_URL =
+  "mongodb+srv://trevor:trevortrinh@cluster0.px6on.mongodb.net/<dbname>?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
+
+/*
+    This make sure we don't have any warning in the console
+*/
+mongoose.set("useFindAndModify", false);
